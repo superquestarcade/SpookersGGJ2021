7bfd5303d536c414939b8586e6d3412617bf2067
NickNightHawk <superquestarcade@gmail.com>
2021-01-28 01:04:42 +1100

    player controller customisation 1

--- SpookersGGJ/Assets/Scripts/PlayerInitialize.cs
+++ SpookersGGJ/Assets/Scripts/PlayerInitialize.cs
@@ -6,13 +6,33 @@ using Mirror;
 public class PlayerInitialize : NetworkBehaviour
 {
     public GameObject playerCameraPrefab;
+    public GameObject playerObj;
+    
+    private GameObject playerCamObj;
+    private PlayerMovement playerMovement;
+    private MouseLook mouseLook;
+
+    /// <summary>
+    /// Setings
+    /// </summary>
+
+    public bool invertMouse = true;
+
+    public Material playerSkinMaterial;
     
     // Start is called before the first frame update
     void Start()
     {
         if (!hasAuthority) return;
 
-        this.gameObject.AddComponent<PlayerMovement>();
-        Instantiate(playerCameraPrefab, this.gameObject.transform);
+        playerMovement = this.gameObject.AddComponent<PlayerMovement>();
+        
+        playerCamObj = Instantiate(playerCameraPrefab, this.gameObject.transform);
+
+        mouseLook = playerCamObj.GetComponent<MouseLook>();
+
+        mouseLook.invertYaxis = invertMouse;
+
+        if (playerSkinMaterial != null && playerObj != null) playerObj.GetComponent<MeshRenderer>().material = playerSkinMaterial;
     }
 }
