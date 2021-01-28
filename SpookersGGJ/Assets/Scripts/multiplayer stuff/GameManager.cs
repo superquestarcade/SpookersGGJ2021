f1ad3a2dd901eb4ad5c9d9cdce5276a3d87fdd9d
saszer <saszer@github.com>
2021-01-29 02:21:17 +1100

    gamemanager + multiplayer

---
+++ SpookersGGJ/Assets/Scripts/multiplayer stuff/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public float HalfTimeAfterSeconds;
+
+    public UnityEvent OnHalfTime;
+    void Start()
+    {
+        RunGame();
+    }
+
+    IEnumerator RunGame()
+    {
+        Debug.Log("GOGOGO");
+
+        yield return new WaitForSecondsRealtime(HalfTimeAfterSeconds);
+
+        Debug.Log("HalfTime");
+        OnHalfTime.Invoke();
+        SkinSwitchandSpawn();
+
+    }
+
+    public GameObject[] Players;
+    void SkinSwitchandSpawn()
+    {
+        Players = GameObject.FindGameObjectsWithTag("GamePlayer");
+
+        foreach (GameObject player in Players)
+        {
+            player.GetComponent<SpawnSkinner>().SwitchSkins();
+        }
+    }
+}
---
+++ SpookersGGJ/Assets/Scripts/multiplayer stuff/SpawnSkinner.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class SpawnSkinner : NetworkBehaviour
+{
+    public player_properties playerproperties;
+    public GameObject GhostSkin;
+    public GameObject HumanSkin;
+
+    GameObject tospawn;
+    GameObject instantiatedskin;
+    private void Start()
+    {
+        CmdSpawn();
+    }
+
+    [Command]
+    void CmdSpawn()
+    {
+        
+        if(playerproperties.teamID == 0)
+        {
+            tospawn = GhostSkin;
+        }
+        else
+        {
+            tospawn = HumanSkin;
+        }
+
+        instantiatedskin = (GameObject)Instantiate(tospawn, tospawn.transform.position, Quaternion.identity);
+        //GameObject owner = this.gameObject;
+        NetworkServer.Spawn(instantiatedskin, connectionToClient);
+    }
+
+    public void SwitchSkins()  // CALL AT HALFTIME OR ANYTIME TO SWITCH AND SPAWN ON SERVER AND ALL!
+    {
+        CmdCallServer();
+    }
+
+    [Command]
+    void CmdCallServer()
+    {
+        DestroyOldSkin();
+    }
+
+    void DestroyOldSkin()
+    {
+        Destroy(instantiatedskin);
+        switchTeamId();
+    }
+
+    void switchTeamId()
+    {
+        if (playerproperties.teamID == 0)
+        {
+            playerproperties.teamID = 1;
+        }
+        else
+        {
+            playerproperties.teamID = 0;
+        }
+
+        CmdSpawn();
+    }
+}
