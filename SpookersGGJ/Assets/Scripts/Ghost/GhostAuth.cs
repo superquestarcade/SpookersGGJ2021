fac201c599306f335ff93a7d45f84cc726240279
NickNightHawk <superquestarcade@gmail.com>
2021-01-31 01:37:04 +1100

    Bandaid for my boi

---
+++ SpookersGGJ/Assets/Scripts/Ghost/GhostAuth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class GhostAuth : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!hasAuthority) return;
+        this.gameObject.AddComponent<GhostAbility>();
+    }
+}
--- SpookersGGJ/Assets/Scripts/UI/UIAudioSliders.cs
+++ SpookersGGJ/Assets/Scripts/UI/UIAudioSliders.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIAudioSliders : MonoBehaviour
 {
+    public Slider sliderFootsteps;
+    public Slider sliderPlaceItem;
+    public Slider sliderPickUp;
+    public Slider sliderPing;
+
+    public int maxPoints = 200;
+    public int pointPool = 0;
+    
+    public void BalanceSliders(AudioTrigger trigger, float value)
+    {
+        if (TotalPoints() > maxPoints)
+        {
+            
+        }
+    }
     public void SetFootstepAudioState(float value)
     {
+        
         AudioManager.singleton.SetAudioFilterState(AudioTrigger.FOOTSTEPS, value);
     }
     
@@ -22,5 +39,24 @@ public class UIAudioSliders : MonoBehaviour
     public void SetPingAudioState(float value)
     {
         AudioManager.singleton.SetAudioFilterState(AudioTrigger.PINGOBJECT, value);
+    }
+
+    int TotalPoints()
+    {
+        int points = 0;
+
+        if (sliderFootsteps.value <= 0.5) points += Mathf.FloorToInt(sliderFootsteps.value * 100);
+        else points += 50 + Mathf.FloorToInt((sliderFootsteps.value-0.5f) * 100);
+        
+        if (sliderPlaceItem.value <= 0.5) points += Mathf.FloorToInt(sliderPlaceItem.value * 100);
+        else points += 50 + Mathf.FloorToInt((sliderPlaceItem.value-0.5f) * 100);
+        
+        if (sliderPickUp.value <= 0.5) points += Mathf.FloorToInt(sliderPickUp.value * 100);
+        else points += 50 + Mathf.FloorToInt((sliderPickUp.value-0.5f) * 100);
+        
+        if (sliderPing.value <= 0.5) points += Mathf.FloorToInt(sliderPing.value * 100);
+        else points += 50 + Mathf.FloorToInt((sliderPing.value-0.5f) * 100);
+
+        return points;
     }
 }
